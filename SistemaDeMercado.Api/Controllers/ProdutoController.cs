1b76d479030f143ce6a88d300ebf5fefa79dbd7d
Isabela Bianca <isabelabianca08@gmail.com>
2023-03-14 13:48:56 -0300

    Implementado a API com os metodos CRUD

--- SistemaDeMercado.Api/Controllers/ProdutoController.cs
+++ SistemaDeMercado.Api/Controllers/ProdutoController.cs
@@ -13,13 +13,65 @@ namespace SistemaDeMercado.Api.Controllers
             _repositorio = repositorio;
         }
 
-
         [HttpGet]
-        public ActionResult <List<Produto>> ObterTodos()
+        public IActionResult ObterTodos()
         {
-            return Ok();
+            try
+            {
+                var obterTodosProdutos = _repositorio.ObterTodos;
+                return Ok(obterTodosProdutos);
+            }
+            catch
+            {
+                throw new Exception("Erro ao obter todos");
+            }
+            
         }
 
+        [HttpGet("{Id}")]
+        public IActionResult ObterPorId(int Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
 
+            var obterPorId = _repositorio.ObterPorId(Id);
+            return Ok(obterPorId);
+        }
+
+        [HttpPost]
+        public IActionResult AdicionarProduto(Produto produto)
+        {
+            _repositorio.AdicionarProduto(produto);
+            return CreatedAtAction("Adicionando produto", produto);
+        }
+
+        [HttpDelete("{Id}")]
+        public IActionResult DeletarProduto(int Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var ProdutoASerDeletado = _repositorio.ObterPorId(Id);
+            _repositorio.DeletarProduto(ProdutoASerDeletado.Id);
+
+            return NoContent();
+        }
+
+        [HttpPut("{Id}")]
+        public IActionResult AtualizarProduto(Produto produto)
+        {
+            if (produto == null)
+            {
+                return NotFound();
+            }
+
+            _repositorio.AtualizarProduto(produto);
+
+            return NoContent();
+        }
     }
 }
--- SistemaDeMercado.Api/Program.cs
+++ SistemaDeMercado.Api/Program.cs
@@ -1,3 +1,5 @@
+using Sistema_de_Mercado;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors();
+builder.Services.AddScoped<IRepositorio, RepositorioLinq2Db>();
 
 var app = builder.Build();
 
