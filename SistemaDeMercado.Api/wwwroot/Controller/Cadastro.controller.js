d4c79bc0e5b1c8febb964879ec0bf4a84b32fa15
Isabela Bianca <isabelabianca08@gmail.com>
2023-03-29 13:50:50 -0300

    Comecando implementacao para salvar novos produtos

--- SistemaDeMercado.Api/wwwroot/Controller/Cadastro.controller.js
+++ SistemaDeMercado.Api/wwwroot/Controller/Cadastro.controller.js
@@ -8,11 +8,13 @@ sap.ui.define([
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Cadastro", {
 
         onInit: function() {
+			
             var modelo = new sap.ui.model.json.JSONModel( {
                 minDate: new Date()
             });
     
             this.getView().setModel(modelo, "ModeloDeData");
+
         },
 
        AoClicarNoBotaoDeVoltar: function () {
@@ -25,6 +27,36 @@ sap.ui.define([
 				var rota = this.getOwnerComponent().getRouter();
 				rota.navTo("listaProduto", {}, true);
 			}
+		},
+
+		aoPressionarCancelar: function() {
+			var rota = this.getOwnerComponent().getRouter();
+				rota.navTo("listaProduto", {}, true);
+		},
+
+			aoPressionarSalvar: async function() {
+			const LabelNomeDoProduto = sap.ui.getCore().byId("LabelNomeDoProduto")
+			const LabelMarcaDoProduto = sap.ui.getCore().byId("LabelMarcaDoProduto").getValue()
+			const LabelCodigoBarrasDoProduto = document.getElementById('codigoBarras')
+			const IdDatePickerDataVencimento = document.getElementById('dataVencimento')
+
+			const produto = {
+				nome: LabelNomeDoProduto.getValue(),
+				marca: LabelMarcaDoProduto.value,
+				codigoBarras: number(LabelCodigoBarrasDoProduto.value),
+				dataVencimento:	IdDatePickerDataVencimento.value
+			}
+
+			//chamar post na API
+			const response = await fetch('https://localhost:7047/api/Produto', {
+				method: 'POST',
+				headers: {
+				  'Accept': 'application/json',
+				  'Content-Type': 'application/json'
+				},
+				body: JSON.stringify(produto)
+			  })
+			const dados = await response.json()
 		}
 	});
 });
