72ad2b24c466dbebd29098a0fd080cbf20a3b9d5
unknown <isabela.castro@inventsoftware.com.br>
2023-05-23 14:17:11 -0300

    Classe de validação funcionando. Agora fazer um metodo para arrumar cache

--- SistemaDeMercado.Api/wwwroot/Controller/Cadastro.controller.js
+++ SistemaDeMercado.Api/wwwroot/Controller/Cadastro.controller.js
@@ -5,10 +5,13 @@ sap.ui.define([
 	"sap/ui/demo/walkthrough/controller/Validacao",
 ], function(Controller, JSONModel, History, Validacao) {
 	"use strict";
-
+	
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Cadastro", {
 
+		_validacao : null,
+
         onInit: function() {
+			this._validacao = new Validacao();
 			var rota = this.getOwnerComponent().getRouter();
 			rota.getRoute("cadastro").attachPatternMatched(this.aoCoincidirRota, this);
 			rota.getRoute("edicao").attachPatternMatched(this.aoCoincidirRota, this);
@@ -75,9 +78,7 @@ sap.ui.define([
 
 		aoPressionarSalvar: function() {
 			const produto = this.getView().getModel("Produto").getData()
-			this._classeValidacao = new Validacao();
-			_classeValidacao.validarProdutos(produto);
-			
+			this._validacao.ValidarProdutos.bind(this)(produto);
 			if(produto.id){
 				let id = produto.id
 				this.editarProduto(produto)
@@ -122,40 +123,6 @@ sap.ui.define([
 			})
 			.then((resposta) => resposta.json())
 			.then(data => data.id);
-		  },
-
-		  validarProdutos: function (produto){
-			let listaDeMensagensDeAviso = [];
-			if (produto.codigoBarras.length !== 13){
-				listaDeMensagensDeAviso += "Codigo de barras deve ter 13 digitos.\n";
-				this.getView().byId("labelCodigoBarras").setValueState("Warning");
-
-			}
-			if(!produto.codigoBarras.startsWith("789"))
-			{
-				listaDeMensagensDeAviso += "O codigo de barras deve começar com '789'.\n";
-				this.getView().byId("labelCodigoBarras").setValueState("Warning");
-			}
-			if(!produto.nome){
-				listaDeMensagensDeAviso += "O nome não pode ser vazio.\n";
-				this.getView().byId("labelNome").setValueState("Warning");
-			}
-			if(!produto.marca){
-				listaDeMensagensDeAviso += "A marca não pode ser vazio.\n";
-				this.getView().byId("labelMarca").setValueState("Warning");
-
-			}
-			if(!produto.dataVencimento){
-				listaDeMensagensDeAviso += "A data de Vencimento não pode ser vazia.\n";
-				this.getView().byId("labelDataVencimento").setValueState("Warning");
-
-			}
-			if(listaDeMensagensDeAviso.length > 0)
-			{
-				this.getView().getModel("ListaAvisos").setProperty("/avisos", listaDeMensagensDeAviso);
-				//this.getView().byId("idMessageTrip").setVisible(true);
-				//this.getView().byId("labelCodigoBarras").setValueStateText("O codigo de barras deve começar com '789'.");
-			}
-		}
+		  }
 	});
 });
--- SistemaDeMercado.Api/wwwroot/Controller/Validacao.js
+++ SistemaDeMercado.Api/wwwroot/Controller/Validacao.js
@@ -1,43 +1,33 @@
 sap.ui.define([
-
-], function() { 
+"sap/ui/base/ManagedObject"
+], function(ManagedObject) { 
 	"use strict";
 
-	return Controller.extend("sap.ui.demo.walkthrough.Validacao", {
+	return ManagedObject.extend("sap.ui.demo.walkthrough.controller.Validacao", {
 
-	_criarModeloDeListaDeAvisos: function(){
-		let modeloDeLista = new JSONModel({
-			avisos: []
-		});
-		this.getView().setModel(modeloDeLista, "ListaAvisos");
-
-	},
-
-
-	validarProdutos: function (produto){
+	ValidarProdutos: function (produto){
 		let listaDeMensagensDeAviso = [];
 		if (produto.codigoBarras.length !== 13){
 			listaDeMensagensDeAviso += "Codigo de barras deve ter 13 digitos.\n";
-			//this.getView().byId("labelCodigoBarras").setValueState("Warning");
-
+			this.getView().byId("labelCodigoBarras").setValueState("Error");
 		}
 		if(!produto.codigoBarras.startsWith("789"))
 		{
 			listaDeMensagensDeAviso += "O codigo de barras deve começar com '789'.\n";
-			//this.getView().byId("labelCodigoBarras").setValueState("Warning");
+			this.getView().byId("labelCodigoBarras").setValueState("Error");
 		}
 		if(!produto.nome){
 			listaDeMensagensDeAviso += "O nome não pode ser vazio.\n";
-			//this.getView().byId("labelNome").setValueState("Warning");
+			this.getView().byId("labelNome").setValueState("Error");
 		}
 		if(!produto.marca){
 			listaDeMensagensDeAviso += "A marca não pode ser vazio.\n";
-			//this.getView().byId("labelMarca").setValueState("Warning");
+			this.getView().byId("labelMarca").setValueState("Error");
 
 		}
 		if(!produto.dataVencimento){
 			listaDeMensagensDeAviso += "A data de Vencimento não pode ser vazia.\n";
-			//this.getView().byId("labelDataVencimento").setValueState("Warning");
+			this.getView().byId("labelDataVencimento").setValueState("Error");
 
 		}
 		if(listaDeMensagensDeAviso.length > 0)
