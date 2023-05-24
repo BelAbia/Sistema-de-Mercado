ef5ed7c4981f8889f9f3ee55577a3eaea630f667
DaviMessias <davibastos451@gmail.com>
2023-05-24 18:21:47 -0300

    Textos i18n configurados na classe de validação

--- SistemaDeMercado.Api/wwwroot/Controller/Cadastro.controller.js
+++ SistemaDeMercado.Api/wwwroot/Controller/Cadastro.controller.js
@@ -2,13 +2,16 @@ sap.ui.define([
 	"sap/ui/core/mvc/Controller",
 	"sap/ui/model/json/JSONModel",
 	"sap/ui/core/routing/History",
-	'sap/m/MessageToast'
-], function(Controller, JSONModel, History, MessageToast) {
+	"sap/ui/demo/walkthrough/controller/Validacao"
+], function(Controller, JSONModel, History, Validacao) {
 	"use strict";
 	
 	return Controller.extend("sap.ui.demo.walkthrough.controller.Cadastro", {
+		
+		_validacao : null,
 
         onInit: function() {
+			this._validacao = new Validacao();
 			var rota = this.getOwnerComponent().getRouter();
 			rota.getRoute("cadastro").attachPatternMatched(this.aoCoincidirRota, this);
 			rota.getRoute("edicao").attachPatternMatched(this.aoCoincidirRota, this);
@@ -18,7 +21,8 @@ sap.ui.define([
 
 		aoCoincidirRota: function (evento) {
 			let conferindoNomeDaRota = evento.getParameter("name") == "cadastro";
-			this._inicializarModelo();
+			this._inicializarModeloDeLista();
+			this._mudandoOEstadoDoInputParaNone();
 			if (conferindoNomeDaRota){
 			this._criarModeloDoProduto();
 
@@ -32,8 +36,8 @@ sap.ui.define([
 			}
 		},
 
-		_inicializarModelo: function (){
-			this._criarModeloDeListaDeAvisos();
+		_inicializarModeloDeLista: function (){
+			this._validacao._criarModeloDeListaDeAvisos.bind(this)();
 		},
 
 		_criarModeloDoProduto: function(){
@@ -47,13 +51,6 @@ sap.ui.define([
 				dataCadastro: dataFormatada.format(dataAtual),
 			});
 			this.getView().setModel(modeloProduto, "Produto");
-		},
-
-		_criarModeloDeListaDeAvisos: function(){
-			let modeloDeLista = new JSONModel({
-				avisos: []
-			});
-			this.getView().setModel(modeloDeLista, "ListaAvisos");
 		},
 
        aoClicarNoBotaoDeVoltar: function () {
@@ -74,10 +71,8 @@ sap.ui.define([
 		},
 
 		aoPressionarSalvar: function() {
-
 			const produto = this.getView().getModel("Produto").getData()
-			this.validarProdutos(produto);
-			
+			this._validacao.ValidarProdutos.bind(this)(produto);
 			if(produto.id){
 				this.editarProduto(produto)
 				var rota = this.getOwnerComponent().getRouter();
@@ -123,38 +118,11 @@ sap.ui.define([
 			.then(data => data.id);
 		  },
 
-		  validarProdutos: function (produto){
-			let listaDeMensagensDeAviso = [];
-			if (produto.codigoBarras.length !== 13){
-				listaDeMensagensDeAviso += "Codigo de barras deve ter 13 digitos.\n";
-				this.getView().byId("labelCodigoBarras").setValueState("Warning");
-
-			}
-			if(!produto.codigoBarras.startsWith("789"))
-			{
-				listaDeMensagensDeAviso += "O codigo de barras deve começar com '789'.\n";
-				this.getView().byId("labelCodigoBarras").setValueState("Warning");
-			}
-			if(!produto.nome){
-				listaDeMensagensDeAviso += "O nome não pode ser vazio.\n";
-				this.getView().byId("labelNome").setValueState("Warning");
-			}
-			if(!produto.marca){
-				listaDeMensagensDeAviso += "A marca não pode ser vazio.\n";
-				this.getView().byId("labelMarca").setValueState("Warning");
-
-			}
-			if(!produto.dataVencimento){
-				listaDeMensagensDeAviso += "A data de Vencimento não pode ser vazia.\n";
-				this.getView().byId("labelDataVencimento").setValueState("Warning");
-
-			}
-			if(listaDeMensagensDeAviso.length > 0)
-			{
-				this.getView().getModel("ListaAvisos").setProperty("/avisos", listaDeMensagensDeAviso);
-				//this.getView().byId("idMessageTrip").setVisible(true);
-				//this.getView().byId("labelCodigoBarras").setValueStateText("O codigo de barras deve começar com '789'.");
-			}
-		}
+		  _mudandoOEstadoDoInputParaNone: function(){
+			this.getView().byId("labelCodigoBarras").setValueState("None");
+			this.getView().byId("labelNome").setValueState("None");
+			this.getView().byId("labelMarca").setValueState("None");
+			this.getView().byId("labelDataVencimento").setValueState("None");
+		  }
 	});
 });
--- SistemaDeMercado.Api/wwwroot/Controller/Validacao.js
+++ SistemaDeMercado.Api/wwwroot/Controller/Validacao.js
@@ -1,43 +1,44 @@
 sap.ui.define([
 	"sap/ui/base/ManagedObject",
+	"sap/ui/model/json/JSONModel"
 
-], function(ManagedObject) { 
+], function (ManagedObject, JSONModel) {
 	"use strict";
-	return Controller.extend("sap.ui.demo.walkthrough.controller.Validacao", {
 
 	return ManagedObject.extend("sap.ui.demo.walkthrough.controller.Validacao", {
 
-	ValidarProdutos: function (produto){
-		let listaDeMensagensDeAviso = [];
-		if (produto.codigoBarras.length !== 13){
-			listaDeMensagensDeAviso += "Codigo de barras deve ter 13 digitos.\n";
-			this.getView().byId("labelCodigoBarras").setValueState("Error");
-		}
-		if(!produto.codigoBarras.startsWith("789"))
-		{
-			listaDeMensagensDeAviso += "O codigo de barras deve começar com '789'.\n";
-			this.getView().byId("labelCodigoBarras").setValueState("Error");
-		}
-		if(!produto.nome){
-			listaDeMensagensDeAviso += "O nome não pode ser vazio.\n";
-			this.getView().byId("labelNome").setValueState("Error");
-		}
-		if(!produto.marca){
-			listaDeMensagensDeAviso += "A marca não pode ser vazio.\n";
-			this.getView().byId("labelMarca").setValueState("Error");
+		_criarModeloDeListaDeAvisos: function () {
+			let modeloDeLista = new JSONModel({
+				avisos: []
+			});
+			this.getView().setModel(modeloDeLista, "ListaAvisos");
+		},
 
-		}
-		if(!produto.dataVencimento){
-			listaDeMensagensDeAviso += "A data de Vencimento não pode ser vazia.\n";
-			this.getView().byId("labelDataVencimento").setValueState("Error");
-
-		}
-		if(listaDeMensagensDeAviso.length > 0)
-		{
-			this.getView().getModel("ListaAvisos").setProperty("/avisos", listaDeMensagensDeAviso);
-			//this.getView().byId("idMessageTrip").setVisible(true);
-			//this.getView().byId("labelCodigoBarras").setValueStateText("O codigo de barras deve começar com '789'.");
-		}
+		ValidarProdutos: function (produto) {
+			let listaDeMensagensDeAviso = [];
+			if (produto.codigoBarras.length !== 13) {
+				listaDeMensagensDeAviso += this.getView().getModel("i18n").getResourceBundle().getText("AvisoTamanhoDoCodigoDeBarras");
+				this.getView().byId("labelCodigoBarras").setValueState("Error");
+			}
+			if (!produto.codigoBarras.startsWith("789")) {
+				listaDeMensagensDeAviso += this.getView().getModel("i18n").getResourceBundle().getText("AvisoPadraoCodigoDeBarras");
+				this.getView().byId("labelCodigoBarras").setValueState("Error");
+			}
+			if (!produto.nome) {
+				listaDeMensagensDeAviso += this.getView().getModel("i18n").getResourceBundle().getText("AvisoNome");
+				this.getView().byId("labelNome").setValueState("Error");
+			}
+			if (!produto.marca) {
+				listaDeMensagensDeAviso += this.getView().getModel("i18n").getResourceBundle().getText("AvisoMarca");
+				this.getView().byId("labelMarca").setValueState("Error");
+			}
+			if (!produto.dataVencimento) {
+				listaDeMensagensDeAviso += this.getView().getModel("i18n").getResourceBundle().getText("AvisoDataVencimento");
+				this.getView().byId("labelDataVencimento").setValueState("Error");
+			}
+			if (listaDeMensagensDeAviso.length > 0) {
+				this.getView().getModel("ListaAvisos").setProperty("/avisos", listaDeMensagensDeAviso);
+			}
 		}
 	});
 });
